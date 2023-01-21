c62cef726a9c602a5bf566d243997c668e9e891a
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-01-21 15:42:06 +0100

    zavrsena paginacija i zvanicni kraj vezbanja zasad

--- timovi-api/controler/task.php
+++ timovi-api/controler/task.php
@@ -221,16 +221,6 @@ if (isset($_GET['timID'])) {
         $response->setData($returnData);
         $response->send();
         exit;
-    } elseif ($_SERVER['REQUEST_METHOD'] === "POST") {
-
-        if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
-            $response = new Response();
-            $response->setHttpStatusCode(400);
-            $response->setSuccess(false);
-            $response->addMessage('Content type header not set to JSON');
-            $response->send();
-            exit;
-        }
     } else {
         $response = new Response();
         $response->setHttpStatusCode(405);
@@ -239,7 +229,87 @@ if (isset($_GET['timID'])) {
         $response->send();
         exit;
     }
-} //ovde pisemo GET all da ispise sve taskove, zato sto nam ne treba id
+} elseif (isset($_GET['page'])) {
+    //kod
+    if ($_SERVER['REQUEST_METHOD'] === "GET") {
+        $page = $_GET['page'];
+
+        if ($page == '' || !is_numeric($page)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage("Page number connot be blank and must be numeric!");
+            $response->send();
+            exit;
+        }
+
+        $limitPerPage = 10;
+        $query = "SELECT count(timID) as totalNoOfTasks FROM tim";
+        $result = $conn->query($query);
+
+        $row = mysqli_fetch_assoc($result);
+        $tasksCount = intval($row['totalNoOfTasks']); //ukupan broj taskova u tabeli
+        // $tasksCount = intval($row['count(id)']);
+        $numOfPages = ceil($tasksCount / $limitPerPage); //zaokruzivanje na veci ceo broj
+        //ako imamo 21 task u tabeli
+        // 1  2  2,1 - NE
+        // 1  2  3 - DA
+        // 10 10 1 - da bi bilo ovako
+
+        if ($numOfPages == 0) {
+            $numOfPages = 1;
+        }
+        // tasks/page/100
+        if ($page > $numOfPages) {
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage('Page not found');
+            $response->send();
+            exit;
+        }
+        //primer da imamo samo 21 task
+        //1 - 1-10
+        //2 - 11-20
+        //3 - 21
+
+        //10*(2-1) = 10
+        //10*(3-1) = 20
+        $offset = ($page == 1 ? 0 : $limitPerPage * ($page - 1));
+        $query = "SELECT * FROM tim limit $limitPerPage offset $offset";
+        $result2 = $conn->query($query);
+
+        $rowCount = $result2->num_rows;
+        while ($row = $result2->fetch_assoc()) {
+            $task = new Task($row['timID'], $row['nazivTima'], $row['drzava'], $row['brojTitula'], $row['godinaOsnivanja']);
+            $taskArray[] = $task->returnTaskArray();
+        }
+
+        $returnData = array();
+        $returnData['row_returned'] = $rowCount;
+        $returnData['total_rows'] = $tasksCount;
+        $returnData['total_pages'] = $numOfPages;
+        $returnData['has_next_page'] = ($page < $numOfPages) ? true : false;
+        $returnData['has_previous_page'] = ($page > 1) ? true : false;
+        $returnData['tasks'] = $taskArray;
+
+        $response = new Response();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->setData($returnData);
+        $response->send();
+        exit;
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage("Request method not allowed");
+        $response->send();
+        exit;
+    }
+}
+
+//ovde pisemo GET all da ispise sve taskove, zato sto nam ne treba id
 elseif (empty($_GET)) {
     if ($_SERVER['REQUEST_METHOD'] === "GET") {
         $query = "SELECT * FROM tim";
@@ -267,6 +337,101 @@ elseif (empty($_GET)) {
         $response = new Response();
         $response->setHttpStatusCode(200);
         $response->setSuccess(true);
+        $response->setData($returnData);
+        $response->send();
+        exit;
+    } elseif ($_SERVER['REQUEST_METHOD'] === "POST") {
+        //da li je application/JSON
+
+        //pogresan tip podataka za objekat koji prosledjuje
+        if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage('Content type header not set to JSON');
+            $response->send();
+            exit;
+        }
+        //da li je JSON format
+        $rawPostData = file_get_contents('php://input');
+        if (!$jsonData = json_decode($rawPostData)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage('Request body is not valid JSON');
+            $response->send();
+            exit;
+        }
+        //da li sva OBAVEZNA polja popunjena
+        //proveravamo da li su setovana obavezna polja
+        if (!isset($jsonData->nazivTima) || !isset($jsonData->drzava) || !isset($jsonData->brojTitula) || !isset($jsonData->godinaOsnivanja)) {
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            if (!isset($jsonData->nazivTima))
+                $response->addMessage('Naziv Tima filed is mandatory and must be provided');
+            if (!isset($jsonData->description))
+                $response->addMessage('Drzava filed is mandatory and must be provided');
+            if (!isset($jsonData->brojTitula))
+                $response->addMessage('Broj Titula filed is mandatory and must be provided');
+            if (!isset($jsonData->godinaOsnivanja))
+                $response->addMessage('Godina Osnivanja filed is mandatory and must be provided');
+            $response->send();
+            exit;
+        }
+
+        $newTask = new Task(
+            null, // za tim id
+            $jsonData->nazivTima,
+            $jsonData->drzava,
+            $jsonData->brojTitula,
+            $jsonData->godinaOsnivanja
+        );
+
+        $nazivTima = $newTask->getNazivTima();
+        $drzava = $newTask->getDrzava();
+        $brojTitula = $newTask->getBrojTitula();
+        $godinaOsnivanja = $newTask->getGodinaOsnivanja();
+
+        $query = "INSERT INTO tim (nazivTima, drzava, brojTitula, godinaOsnivanja) VALUES ('$nazivTima', '$drzava', '$brojTitula','$godinaOsnivanja')";
+        $result = $conn->query($query);
+
+        $rowCount = $conn->affected_rows;
+        if ($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Task not saved");
+            $response->send();
+            exit;
+        }
+
+        $lastTaskId = $conn->insert_id;
+
+        $query2 = "SELECT * FROM tim WHERE timID=$lastTaskId";
+        $result2 = $conn->query($query2);
+
+        $rowCount = $result2->num_rows;
+        if ($rowCount == 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Failed to retive task after creation");
+            $response->send();
+            exit;
+        }
+
+        $row = $result2->fetch_assoc();
+        $task = new Task($row['timID'], $row['nazivTima'], $row['drzava'], $row['brojTitula'], $row['godinaOsnivanja']);
+
+        $returnData = array();
+        $returnData['rows_returned'] = $rowCount;
+        $returnData['task'] = $task->returnTaskArray();
+
+        $response = new Response();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->addMessage("Task creted");
         $response->setData($returnData);
         $response->send();
         exit;
